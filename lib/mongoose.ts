2cf6702d582fc15a1f93c29f7aba2b73f2858644
hibahdesu <hibahsindi@gmail.com>
2024-12-13 21:23:01 +0300

    connect

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,8 +1,9 @@
+//lib/mongoose
 import mongoose from 'mongoose';
 
 const connectToDatabase = async () => {
   if (mongoose.connection.readyState >= 1) {
-    console.log('MongoDB is already connected');
+    // If already connected or in the process of connecting, return early
     return;
   }
 
@@ -13,14 +14,8 @@ const connectToDatabase = async () => {
       throw new Error('MongoDB URI is not defined in environment variables.');
     }
 
-    console.log('Connecting to MongoDB...');
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      connectTimeoutMS: 10000, // Timeout for establishing connection
-      socketTimeoutMS: 45000,  // Timeout for inactivity, i.e., socket gets closed after 45 seconds of inactivity
-      serverSelectionTimeoutMS: 5000, // Timeout for selecting the server, useful in case of network latency
-    });
+    // Connect to MongoDB without the deprecated options
+    await mongoose.connect(mongoUri);
 
     console.log('MongoDB connected');
   } catch (error) {
